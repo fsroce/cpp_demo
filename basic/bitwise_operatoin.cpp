e85f20bdf4b4515aa49f30966a0d4fe317ba5268
fsrice <harlanlambert@gmail.com>
2024-04-21 15:34:41 +0800

    basic

---
+++ basic/bitwise_operatoin.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+/*
+ * @description 位运算符
+*/
+int main () {
+  /*
+   * & 与
+   * | 或
+   * ^ 异或
+   * ~ 取反
+   * << 左移
+   * >> 右移
+  */
+  cout << (~1) << endl;// -2
+  cout << (~0) << endl;// -1
+  return 0;
+}
---
+++ basic/complement_code.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+/*
+ * @description 补码相关
+*/
+int main () {
+  return 0;
+}
